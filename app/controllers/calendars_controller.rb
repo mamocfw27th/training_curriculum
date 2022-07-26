2cecfae36e35253d28f7245365180a39e77c9c1b
mamocfw27th <108714748+mamocfw27th@users.noreply.github.com>
2022-07-26 22:03:50 +0900

    issue６

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -38,7 +38,7 @@ class CalendarsController < ApplicationController
 
       wday_num = Date.today.wday
       wday_num = wday_num + x
-      if wday_num + 1 > 7
+      if wday_num + 1 >= 7
         wday_num = wday_num -7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
